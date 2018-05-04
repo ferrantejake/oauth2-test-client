39acd75fa462c14d778b3bfa599f755acdfb4b3f
Jake Ferrante <jake.ferrante@hotmail.com>
2018-05-04 12:46:01 -0400

    Clean up environment variables Update master config

--- src/components/passport.ts
+++ src/components/passport.ts
@@ -4,23 +4,23 @@ import { Strategy as OAuth2Strategy } from 'passport-oauth2';
 
 export function init() {
     const {
-        YO_AUTHORIZATION_ENDPOINT,
-        YO_TOKEN_ENDPOINT,
+        AUTHORIZATION_ENDPOINT,
+        TOKEN_ENDPOINT,
         CLIENT_ID,
         CLIENT_SECRET,
         PORT
       } = process.env;
 
     console.log('vars');
-    console.log('YO_AUTHORIZATION_ENDPOINT', YO_AUTHORIZATION_ENDPOINT);
-    console.log('YO_TOKEN_ENDPOINT', YO_TOKEN_ENDPOINT);
+    console.log('AUTHORIZATION_ENDPOINT', AUTHORIZATION_ENDPOINT);
+    console.log('TOKEN_ENDPOINT', TOKEN_ENDPOINT);
     console.log('CLIENT_ID', CLIENT_ID);
     console.log('CLIENT_SECRET', CLIENT_SECRET);
     console.log('PORT', PORT);
 
     passport.use(new OAuth2Strategy({
-        authorizationURL: YO_AUTHORIZATION_ENDPOINT,
-        tokenURL: YO_TOKEN_ENDPOINT,
+        authorizationURL: AUTHORIZATION_ENDPOINT,
+        tokenURL: TOKEN_ENDPOINT,
         clientID: CLIENT_ID,
         clientSecret: CLIENT_SECRET,
         callbackURL: `http://localhost:${PORT}/auth/callback`
